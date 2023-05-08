a044c70f4aecfc87ef5be99bd660442c06f46b60
202116302 <wls5258@naver.com>
2023-05-08 19:55:42 +0900

    새 정보 업데이트 (중복제거) 코드 수정

--- crewling.py
+++ crewling.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
 import os
+import array as np
 
 now = datetime.now()
 today = now.date()
@@ -25,6 +26,8 @@ def main():
     agency = []
     # URL
     site = []
+    # 번호
+    number = []
 
     origin = "https://icals.snu.ac.kr"
 
@@ -34,7 +37,6 @@ def main():
             url = origin + f"/board/subject/page/{i}?pmove=1"
             web = requests.get(url)
             soup = BeautifulSoup(web.content, 'html.parser')
-
             # 테이블 행 마다 가져오기(컬럼 행 제외)
             notion = soup.table.find_all('tr')[1:]
             for i in notion:
@@ -43,12 +45,12 @@ def main():
                 date_result = datetime.strptime(date[-1], datetime_format)
                 if date_result.date() >= today:
                     content = i.find_all('td', class_="alignLeft")
+                    number.append(i.select_one("td:nth-of-type(1)").text)
                     # 상세페이지 들어가서 정보 가져오기
                     for x in content:
                         link = x.find('a')['href']
                         notion_pages = requests.get(origin + link)
                         notion_soup = BeautifulSoup(notion_pages.content, 'html.parser')  # 상세페이지 들어가기
-
                         title_c = notion_soup.find('dl', class_='cHeader')  # 제목 가져오기
                         title.append(title_c.find('dt').text)
                         data = notion_soup.find_all('li', class_='custom_text')  # 신청기간, 지원기관, URL 가져오기 (같은 태그임)
@@ -61,49 +63,58 @@ def main():
                             else:
                                 site.append(con[1])
 
-        df = pd.DataFrame({'제목': title, '신청기간': app, '지원기관': agency, 'URL': site})
-        df.to_excel('output\projects.xlsx')
+        df = pd.DataFrame({'번호': number, '제목': title, '신청기간': app, '지원기관': agency, 'URL': site})
+        df.to_excel('output\projects.xlsx', index=False)
 
     # 2회 이상 실행할 때 (새글 알림)
     else:
         df = pd.read_excel('output\projects.xlsx')
-        url = origin + f"/board/subject/page/1?pmove=1"
-        web = requests.get(url)
-        soup = BeautifulSoup(web.content, 'html.parser')
+        for i in range(1, 5):
+            url = origin + f"/board/subject/page/{i}?pmove=1"
+            web = requests.get(url)
+            soup = BeautifulSoup(web.content, 'html.parser')
+            nums = list(df['번호'].unique())
 
-        # 테이블 행 마다 가져오기(컬럼 행 제외)
-        notion = soup.table.find_all('tr')[1:]
-        for i in notion:
-            content = i.find_all('td', class_="alignLeft")
-            for q in content:
-                link = q.find('a')['href']
-                notion_pages = requests.get(origin + link)
-                notion_soup = BeautifulSoup(notion_pages.content, 'html.parser')
-                title_c = notion_soup.find('dl', class_='cHeader')  # 제목 가져오기
-                if title_c in df['제목']:
-                    pass
-                else:
-                    # 크롤링 했을 때 엑셀파일에 중복되지 않은 공지가 있으면 알림, 새로 저장
-                    title.append(title_c.find('dt').text)
-                    data = notion_soup.find_all('li', class_='custom_text')  # 신청기간, 지원기관, URL 가져오기 (같은 태그임)
-                    for z in data:
-                        con = z.text.replace('\t', '').split('\n')  # 분류, 공백 정리
-                        if con[0] == '신청기간':
-                            app.append(con[1])
-                        elif con[0] == '지원기관':
-                            agency.append(con[1])
+            # 테이블 행 마다 가져오기(컬럼 행 제외)
+            notion = soup.table.find_all('tr')[1:]
+            for i in notion:
+                date = i.select_one("td:nth-of-type(3)").text.split('-')
+                date_result = datetime.strptime(date[-1], datetime_format)
+                if date_result.date() >= today:
+                    content = i.find_all('td', class_="alignLeft")
+                    num = i.select_one("td:nth-of-type(1)").text
+                    number.append(num)
+                    for q in content:
+                        link = q.find('a')['href']
+                        notion_pages = requests.get(origin + link)
+                        notion_soup = BeautifulSoup(notion_pages.content, 'html.parser')
+                        title_c = notion_soup.find('dl', class_='cHeader')  # 제목 가져오기
+                        if int(num) in nums:
+                            pass
                         else:
-                            site.append(con[1])
+                            # 크롤링 했을 때 엑셀파일에 중복되지 않은 공지가 있으면 알림, 새로 저장
+                            title.append(title_c.find('dt').text)
+                            data = notion_soup.find_all('li', class_='custom_text')  # 신청기간, 지원기관, URL 가져오기 (같은 태그임)
+                            for z in data:
+                                con = z.text.replace('\t', '').split('\n')  # 분류, 공백 정리
+                                if con[0] == '신청기간':
+                                    app.append(con[1])
+                                elif con[0] == '지원기관':
+                                    agency.append(con[1])
+                                else:
+                                    site.append(con[1])
 
-            if len(title) > 0:
-                # 새로운 연구과제 공지 알림
-                requests.post("https://ntfy.sh/re_project_SF",
-                              data=f'연구과제공모 새 글 공지 :{title}'.encode(encoding='utf-8'))
-                df2 = pd.DataFrame({'제목': title, '신청기간': app, '지원기관': agency, 'URL': site})
-                df3 = pd.concat([df2, df])
-                df3.to_excel('output/projects.xlsx')
-            else:
-                pass
+        print(len(title))
+
+        if len(title) > 0:
+            # 새로운 연구과제 공지 알림
+            requests.post("https://ntfy.sh/re_project_SF",
+                          data=f'연구과제공모 새 글 공지 :{title}'.encode(encoding='utf-8'))
+            df2 = pd.DataFrame({'제목': title, '신청기간': app, '지원기관': agency, 'URL': site})
+            df3 = pd.concat([df2, df])
+            df3.to_excel('output/projects.xlsx', index=False)
+        else:
+            pass
 
 
 if __name__ == '__main__':
